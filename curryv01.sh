bff02647b493a0f0f76ef98be0a59c5556931859
Darexxx <41051996+Darexxx@users.noreply.github.com>
2018-07-12 13:29:35 +0700

    Add files via upload

---
+++ curryv01.sh
@@ -0,0 +1,46 @@
+#!/system/xbin/bash
+clear
+toilet -f mono12 -F gay "Curry"
+sleep 1
+echo "\033[31;1m Mana Saia Tau"
+sleep 1
+echo "\033[31;1m Coded by ./D@r3XxX"
+sleep 1
+echo "\033[36;1m Pilih Tools Goblok :"
+echo "1.Tokopedia"
+echo "2.PHD"
+echo "3.jd.id"
+echo "0.Keluar"
+echo "\033[33;1m Pilih Angka"
+read curry
+if [ $curry = 1 ] || [ $curry = 1 ]
+then
+clear
+echo "\033[34;1m"
+figlet "Tokopedia"
+php 1.php
+fi
+
+if [ $curry = 2 ] || [ $curry = 2 ]
+then
+clear
+figlet "PHD"
+echo "\033[30;1m"
+php 4.php
+fi
+
+if [ $curry = 3 ] || [ $curry = 3 ]
+then
+clear
+figlet "JD.ID"
+echo "\033[30;1m"
+php 5.php
+fi
+
+if [ $curry  = 0 ] || [ $curry = 0 ]
+then
+echo "\033[31;1m keluar"
+sleep 1
+echo "\033[32;1m Bye FuckingPeople"
+sleep 1
+fi
